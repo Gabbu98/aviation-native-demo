947cc6ab47a40f11ba4b5b0cfdc8617463864456
gabriel <camillerigabriel43@gmail.com>
2024-01-22 21:30:08 +0100

    native executable

--- src/main/java/com/demo/aviation/AircraftService.java
+++ src/main/java/com/demo/aviation/AircraftService.java
@@ -2,14 +2,27 @@ package com.demo.aviation;
 
 import com.demo.aviation.models.Aircraft;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AircraftService {
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     public Aircraft createAircraft(){
         String json = "{ \"registrationNumber\" : \"9hian\", \"type\" : \"sira\" , \"model\" : \"sira\" }";
-        Aircraft car = new Aircraft("sss","ss","ss");
+
+        Aircraft car = null;
+        try {
+            System.out.println(objectMapper);
+            car = objectMapper.readValue(json, Aircraft.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return car;
     }
 }
